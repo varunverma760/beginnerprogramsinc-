ed4508399f3396adc711a0a71367652269da350f
Varun Verma <56150770+varunverma760@users.noreply.github.com>
2020-10-21 07:16:26 +0530

    Add files via upload

---
+++ conversion int to float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void) {
+	float f_num;
+	int i_num;
+	printf(" \n Enter the any integer value:");
+	scanf("%d",&i_num);
+	f_num = i_num;
+	printf(" \n the floating point variant %d = %f",i_num,f_num);
+	// your code goes here
+	return 0;
+}
